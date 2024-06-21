f3bbcfda9dfb63022f6815d02045818a66bfd192
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-21 17:28:33 +0200

    feat: :sparkles: add the ChatView component inside the app.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+import ChatView from '@/views/ChatView.vue'
 </script>
 
 <template>
   <header>
     <HelloWorld msg="You did it!" />
   </header>
+  <ChatView />
 </template>
