7e968064ddf65535fb1e4e2a8accdbe1db48bea5
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-21 17:29:45 +0200

    feat: :sparkles: add the ChatMessages component

---
+++ src/components/chat/ChatMessages.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex-1 overflow-y-auto p-4">
+    <div class="flex flex-col space-y-2">
+      <ChatBubble v-for="message in messages" :key="message.id" v-bind="message" />
+      <!-- :message="message.message"
+        :isMine="message.isMine"
+        :image="message.image" -->
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { ChatMessage } from '@/interfaces/chat-message.interface'
+import ChatBubble from './ChatBubble.vue'
+
+interface Props {
+  messages: ChatMessage[]
+}
+
+defineProps<Props>()
+</script>
