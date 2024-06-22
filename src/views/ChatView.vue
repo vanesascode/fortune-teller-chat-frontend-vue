aef85af607e9583f390ea2f8927826367fcae276
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-22 14:40:11 +0200

    refactor: :recycle: remove the messages and the logic to create new ones from the chatview file to the useChat composable

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,41 +1,21 @@
 <template>
-  <div class="bg-gray-100 h-screen flex flex-col max-w-lg mx-auto">
-    <div class="bg-blue-500 p-4 text-white flex justify-between items-center">
-      <span>Mi esposa</span>
-    </div>
+  <div class="bg-gray-100 h-screen flex flex-col max-w-lg mx-auto rounded-lg">
+    <p
+      class="bg-black p-4 text-white flex justify-center items-center sm:rounded-t-lg sm:text-xl text-center"
+    >
+      <span>Ask <b>Yes</b> or <b>No</b> questions to your fortune-teller:</span>
+    </p>
 
     <ChatMessages :messages="messages" />
 
-    <ChatBox @send-message="onMessage($event)" />
+    <ChatInput @send-message="onMessage($event)" />
   </div>
 </template>
 
 <script setup lang="ts">
 import ChatMessages from '@/components/chat/ChatMessages.vue'
-import ChatBox from '@/components/chat/ChatBox.vue'
-import type { ChatMessage } from '@/interfaces/chat-message.interface'
-import { ref } from 'vue'
+import ChatInput from '@/components/chat/ChatInput.vue'
+import { useChat } from '@/composables/useChat'
 
-const messages = ref<ChatMessage[]>([
-  {
-    id: new Date().getTime(),
-    isMine: true,
-    message: 'Hello, how are you?'
-  },
-  {
-    id: new Date().getTime() + 1,
-    isMine: false,
-    message: 'Bad',
-    image:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
-  }
-])
-
-const onMessage = (text: string) => {
-  messages.value.push({
-    id: new Date().getTime(),
-    isMine: true,
-    message: text
-  })
-}
+const { messages, onMessage } = useChat()
 </script>
