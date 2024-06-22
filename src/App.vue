97cbe3c33f30549d3b1cc26f097d06c324a60480
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-22 14:36:19 +0200

    refactor: :recycle: remove the HelloWorld component from the app vue file

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
+<template>
+  <body>
+    <ChatView />
+  </body>
+</template>
+
 <script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue'
 import ChatView from '@/views/ChatView.vue'
 </script>
-
-<template>
-  <header>
-    <HelloWorld msg="You did it!" />
-  </header>
-  <ChatView />
-</template>
