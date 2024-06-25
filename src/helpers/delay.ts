65744d6d6d9b7ea446092f04fa273417858ebacc
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-25 16:17:28 +0200

    refactor: :recycle: add the trailing commas to the delay helper file

--- src/helpers/delay.ts
+++ src/helpers/delay.ts
@@ -1,6 +1,6 @@
 export const delay = () =>
   new Promise((resolve) => {
-    const RANDOM_SECONDS = Math.floor(Math.random() * 10) * 1000
-    console.log(`Delaying for ${RANDOM_SECONDS}ms`)
-    setTimeout(resolve, RANDOM_SECONDS)
-  })
+    const RANDOM_SECONDS = Math.floor(Math.random() * 10) * 1000;
+    console.log(`Delaying for ${RANDOM_SECONDS}ms`);
+    setTimeout(resolve, RANDOM_SECONDS);
+  });
