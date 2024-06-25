a735a51651cd8226fc51083705515a40dfa4eeda
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-25 16:15:23 +0200

    refactor: :recycle: add the trailing commas to the main ts file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import './assets/styles/styles.scss'
+import './assets/styles/styles.scss';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
