c6ddf6501c4d9477e751134d46fd82267ed46afb
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-25 16:38:13 +0200

    refactor: :recycle: add trailing commas to the chatbubble component test file

--- tests/components/ChatBubble.spec.ts
+++ tests/components/ChatBubble.spec.ts
@@ -1,69 +1,69 @@
-import { mount } from '@vue/test-utils'
-import ChatBubble from '@/components/chat/ChatBubble.vue'
-import { describe, expect, test } from 'vitest'
+import { mount } from '@vue/test-utils';
+import ChatBubble from '@/components/chat/ChatBubble.vue';
+import { describe, expect, test } from 'vitest';
 
 const mountChatBubbleComponent = (message: string, isMine: boolean, image?: string) => {
   return mount(ChatBubble, {
     props: {
       message,
       isMine,
-      image
-    }
-  })
-}
+      image,
+    },
+  });
+};
 
-const MESSAGE = 'Hello, World!'
+const MESSAGE = 'Hello, World!';
 
 describe('<ChatBubble tests/>', () => {
   test('ChatBubble renders correctly', () => {
-    const wrapper = mountChatBubbleComponent(MESSAGE, true)
-    expect(wrapper.html()).toMatchSnapshot()
-  })
+    const wrapper = mountChatBubbleComponent(MESSAGE, true);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
 
   test('ChatBubble renders correctly when isMine is false', () => {
-    const wrapper = mountChatBubbleComponent(MESSAGE, false, 'https://image.jpg')
-    expect(wrapper.html()).toMatchSnapshot()
-  })
+    const wrapper = mountChatBubbleComponent(MESSAGE, false, 'https://image.jpg');
+    expect(wrapper.html()).toMatchSnapshot();
+  });
 
   test('Find elements by classes when isMine is true', () => {
-    const wrapper = mountChatBubbleComponent(MESSAGE, true)
+    const wrapper = mountChatBubbleComponent(MESSAGE, true);
 
-    expect(wrapper.find('.bg-blue-200').exists()).toBeTruthy()
-    expect(wrapper.find('.bg-gray-300').exists()).toBeFalsy()
-  })
+    expect(wrapper.find('.bg-blue-200').exists()).toBeTruthy();
+    expect(wrapper.find('.bg-gray-300').exists()).toBeFalsy();
+  });
 
   test('Find elements by classes when isMine is false', () => {
-    const wrapper = mountChatBubbleComponent(MESSAGE, false)
+    const wrapper = mountChatBubbleComponent(MESSAGE, false);
 
-    expect(wrapper.find('.bg-blue-200').exists()).toBeFalsy()
-    expect(wrapper.find('.bg-gray-300').exists()).toBeTruthy()
-  })
+    expect(wrapper.find('.bg-blue-200').exists()).toBeFalsy();
+    expect(wrapper.find('.bg-gray-300').exists()).toBeTruthy();
+  });
 
   test('Message shown is the same as the prop when isMine is false', () => {
-    const wrapper = mountChatBubbleComponent(MESSAGE, false)
+    const wrapper = mountChatBubbleComponent(MESSAGE, false);
 
-    expect(wrapper.text()).toBe(MESSAGE)
-    expect(wrapper.find('span').text()).toContain(MESSAGE)
-  })
+    expect(wrapper.text()).toBe(MESSAGE);
+    expect(wrapper.find('span').text()).toContain(MESSAGE);
+  });
 
   test('Message shown is the same as the prop when isMine is true', () => {
-    const wrapper = mountChatBubbleComponent(MESSAGE, true)
+    const wrapper = mountChatBubbleComponent(MESSAGE, true);
 
-    const [, secondDiv] = wrapper.findAll('div')
+    const [, secondDiv] = wrapper.findAll('div');
 
-    expect(secondDiv.text()).toContain(MESSAGE)
-  })
+    expect(secondDiv.text()).toContain(MESSAGE);
+  });
 
   test('Image does not exist when isMine is true', () => {
-    const wrapper = mountChatBubbleComponent(MESSAGE, true)
+    const wrapper = mountChatBubbleComponent(MESSAGE, true);
 
-    expect(wrapper.find('img').exists()).toBeFalsy()
-  })
+    expect(wrapper.find('img').exists()).toBeFalsy();
+  });
 
   test('Image exists when isMine is false', () => {
-    const wrapper = mountChatBubbleComponent(MESSAGE, false, 'https://image.jpg')
+    const wrapper = mountChatBubbleComponent(MESSAGE, false, 'https://image.jpg');
 
-    expect(wrapper.find('img').exists()).toBeTruthy()
-    expect(wrapper.find('img').attributes('src')).toBe('https://image.jpg')
-  })
-})
+    expect(wrapper.find('img').exists()).toBeTruthy();
+    expect(wrapper.find('img').attributes('src')).toBe('https://image.jpg');
+  });
+});
